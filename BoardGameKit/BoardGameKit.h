6e71fa05c23651a98e12facbbcacab9afdcb325a
Arbel Israeli <arbel03@gmail.com>
2018-05-02 17:46:27 +0300

    Initial Commit

---
+++ BoardGameKit/BoardGameKit.h
@@ -0,0 +1,19 @@
+//
+//  BoardGameKit.h
+//  BoardGameKit
+//
+//  Created by Arbel Israeli on 02/05/2018.
+//  Copyright © 2018 Arbel Israeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoardGameKit.
+FOUNDATION_EXPORT double BoardGameKitVersionNumber;
+
+//! Project version string for BoardGameKit.
+FOUNDATION_EXPORT const unsigned char BoardGameKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoardGameKit/PublicHeader.h>
+
+
